351f53e020fcbbd86fd1de8e8c62d1f7858e9414
Abdullah Uzundere <auzundere@3rdeyecam.com>
2020-01-28 02:00:20 -0600

    initial

---
+++ movie-info-service/src/main/java/com/uzundere/movieinfoservice/resources/MovieDataResource.java
@@ -0,0 +1,16 @@
+package com.uzundere.movieinfoservice.resources;
+
+import com.uzundere.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieDataResource {
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+            return new Movie(movieId, "Test name");
+    }
+}
